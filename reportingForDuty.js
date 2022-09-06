4bf81845b2079124086dc9fd8faf0c61d5b92e6d
smw4 <7003183+smw4@users.noreply.github.com>
2022-09-06 14:12:37 -0400

    reportingforDuty() working.

---
+++ reportingForDuty.js
@@ -0,0 +1,9 @@
+//Write a function, reportingForDuty(), that has two string parameters, rank and lastName, and returns a string in the following format: ‘rank lastName reporting for duty!’
+
+const reportingForDuty = (rank, lastName) => {
+	return `${rank} ${lastName} reporting for duty!`;
+};
+
+// console.log(reportingForDuty('Private', 'Fido')) // Should return 'Private Fido reporting for duty!'
+console.log(reportingForDuty('Private', 'Fido'));
+// We encourage you to add more function calls of your own to test your code!
