2406f4859afb94cf56d36571884f0a9c44a0343f
smw4 <7003183+smw4@users.noreply.github.com>
2022-09-06 14:35:59 -0400

    sillySentence() complete

---
+++ sillySentence().js
@@ -0,0 +1,8 @@
+// Write your function here:
+
+const sillySentence = (adjective, verb, noun) => {
+	return `I am so ${adjective} because I ${verb} coding! Time to write some more awesome ${noun}!`;
+};
+
+// Uncomment the line below when you're ready to try out your function
+console.log(sillySentence('excited', 'love', 'functions'));
